0d9b52872299b7cbe764507741bd9b1c92045be7
Zaid Kindman <38330943+zaid1997@users.noreply.github.com>
2020-03-19 19:55:53 +0300

    Add files via upload

---
+++ SocketClient/SocketClient/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class SocketClient
+{
+    IPHostEntry host = Dns.GetHostEntry("localhost");
+    public static IPAddress ipAddress = System.Net.IPAddress.Parse("192.168.3.147");
+    public static IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5050);
+    public static Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
+    public static int Main(String[] args)
+    {
+        StartClient();
+        return 0;
+    }
+
+    public static void SendMessage()
+    {
+        string inputstring = Console.ReadLine();
+        byte[] msg = Encoding.ASCII.GetBytes(inputstring);
+        sender.Send(msg);
+    }
+
+    public static void ReceiveMessage()
+    {
+        byte[] bytes = new byte[1024];
+        int bytesRec = sender.Receive(bytes);
+        Console.WriteLine("Server: {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+    }
+
+    public static void StartClient()
+    {
+        byte[] bytes = new byte[1024];
+
+        try
+        {
+            try
+            {
+                sender.Connect(remoteEP);
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                while (true)
+                {
+                    Thread receive = new Thread(ReceiveMessage);
+                    receive.Start();
+                    SendMessage();
+                }
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
---
+++ SocketListener/SocketListener/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class SocketListener
+{
+    public static int Main(String[] args)
+    {
+        StartServer();
+        return 0;
+    }
+
+    public static IPAddress ipAddress = System.Net.IPAddress.Parse("192.168.3.147");
+    public static IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5050);
+    public static Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    public static Socket handler;
+
+    public static void ReceiveMessage()
+    {
+        try
+        {
+            while(true)
+            {
+                string data = null;
+                byte[] bytes = null;
+                bytes = new byte[1024];
+
+                int bytesRec = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                Console.WriteLine("Client: {0}", data);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public static void StartServer()
+    {
+        listener.Bind(localEndPoint);
+        listener.Listen(10);
+        Console.WriteLine("Waiting for a connection...");
+
+        try
+        {
+            handler = listener.Accept();
+            string inputstring;
+
+            while (true)
+            {
+                Thread receive = new Thread(ReceiveMessage);
+                receive.Start();
+
+                inputstring = Console.ReadLine();
+                byte[] msg = Encoding.ASCII.GetBytes(inputstring);
+                handler.Send(msg);
+            }
+
+            /*
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            handler.Send(msg);
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+            */
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\n Press any key to continue...");
+        Console.ReadKey();
+    }
+}
